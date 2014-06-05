691719c9ad78f102ed178c132fdb2ae8d4f2809d
Slawomir Borowiec <przodownik@tlen.pl>
2014-06-05 15:08:07 +0200

    -

--- src/main/java/pl/java/scalatech/config/JpaConfig.java
+++ src/main/java/pl/java/scalatech/config/JpaConfig.java
@@ -1,5 +1,8 @@
 package pl.java.scalatech.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
 import net.sf.log4jdbc.Log4jdbcProxyDataSource;
@@ -9,11 +12,13 @@ import net.sf.log4jdbc.tools.LoggingType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -25,13 +30,14 @@ import com.jolbox.bonecp.BoneCPDataSource;
 
 /**
  * @author przodownik
- * Module name :    JpaKata
- * Creating time :  30 maj 2014
+ *         Module name : JpaKata
+ *         Creating time : 30 maj 2014
  */
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = "pl.java.scalatech.repository")
 @EntityScan(basePackages = "pl.java.scalatech.entity")
 @PropertySource("classpath:spring-data.properties")
+@Profile("dev")
 public class JpaConfig {
 
     @Value("${dataSource.driverClassName}")
@@ -45,15 +51,17 @@ public class JpaConfig {
     @Value("${hibernate.dialect}")
     private String dialect;
     @Value("${hibernate.hbm2ddl.auto}")
-    private String hbm2ddlAuto;
+    private Boolean hbm2ddlAuto;
     @Value("${boneCp.partition.count}")
     private int partitionCount;
     @Value("${boneCp.partition.minConnectionsPerPartition}")
     private int minConnectionsPerPartition;
     @Value("${boneCp.partition.maxConnectionsPerPartition}")
     private int maxConnectionsPerPartition;
+    @Value("${hibernate.show.sql}")
+    private Boolean showSql;
 
-    @Bean
+    @Bean(destroyMethod = "close")
     public DataSource dataSourceOrginal() {
         BoneCPDataSource boneCPDataSource = new BoneCPDataSource();
         boneCPDataSource.setDriverClass(driver);
@@ -72,7 +80,13 @@ public class JpaConfig {
         dataSource.setLogFormatter(logFormater());
         return dataSource;
     }
-
+    
+    @Bean
+    @Profile("dev")
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL).build();
+    }
+    
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new JpaTransactionManager();
@@ -83,20 +97,32 @@ public class JpaConfig {
         return new PersistenceExceptionTranslationPostProcessor();
     }
 
+    public Map<String, Object> jpaProperties() {
+        Map<String, Object> props = new HashMap<>();
+        // TODO
+        props.put("hibernate.cache.use_query_cache", "true");
+        props.put("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
+        props.put("hibernate.cache.provider_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
+        props.put("hibernate.cache.use_second_level_cache", "true");
+
+        return props;
+    }
+
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
         lef.setDataSource(dataSource(dataSourceOrginal()));
         lef.setJpaVendorAdapter(jpaVendorAdapter());
-        lef.setPackagesToScan("pl.java.scalatech.entity");
+        lef.setJpaPropertyMap(jpaProperties());
+        lef.setPackagesToScan("pl.java.scalatech.entity"); //eliminate persistence.xml
         return lef;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
         HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        hibernateJpaVendorAdapter.setShowSql(false);
-        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setShowSql(showSql);
+        hibernateJpaVendorAdapter.setGenerateDdl(hbm2ddlAuto);
         hibernateJpaVendorAdapter.setDatabase(Database.HSQL);
         hibernateJpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.HSQLDialect");
         return hibernateJpaVendorAdapter;
