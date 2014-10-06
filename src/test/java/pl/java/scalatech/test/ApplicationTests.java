939a09af65faa6178117ca13fdf4c00d98a55faa
Slawomir Borowiec <przodownik@tlen.pl>
2014-10-06 14:05:29 +0200

    audit added

--- src/test/java/pl/java/scalatech/test/ApplicationTests.java
+++ src/test/java/pl/java/scalatech/test/ApplicationTests.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import pl.java.scalatech.app.JpaKataApplication;
 import pl.java.scalatech.config.JpaConfig;
 import pl.java.scalatech.entity.Customer;
 import pl.java.scalatech.repository.CustomerRepository;
+import pl.java.scalatech.service.customer.CustomerService;
 
 /**
  * @author przodownik
@@ -24,12 +26,12 @@ import pl.java.scalatech.repository.CustomerRepository;
  *         Creating time : 30 maj 2014
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {JpaKataApplication.class,  JpaConfig.class })
-@ActiveProfiles(value="dev")
+@SpringApplicationConfiguration(classes = { JpaKataApplication.class, JpaConfig.class })
+@ActiveProfiles(value = "dev")
 @Transactional
 public class ApplicationTests {
     @Autowired
-    private CustomerRepository customerRepository;
+    private CustomerService customerService;
 
     @Test
     public void contextLoads() {
@@ -38,10 +40,10 @@ public class ApplicationTests {
 
     @Test
     public void shouldInsertAndRetrieveData() throws InterruptedException {
-        customerRepository.save(Customer.builder().login("przodownik").name("slawek").salary(new BigDecimal(2342)).build());
-        List<Customer> customers = customerRepository.findAll();
-        "d".getBytes();
+        customerService.persistCustomer(Customer.builder().login("przodownik").name("slawek").salary(new BigDecimal(2342)).build());
+        List<Customer> customers = customerService.getAllCustomers(new PageRequest(0, 1)).getContent();
+
         Assertions.assertThat(customers).isNotEmpty().hasSize(1);
-        Thread.sleep(100000);
+
     }
 }
