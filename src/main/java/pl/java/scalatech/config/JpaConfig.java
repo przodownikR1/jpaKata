13859875171bce02c94b2e2b151513b0313ee9ea
Slawomir Borowiec <przodownik@tlen.pl>
2014-10-06 17:01:48 +0200

    h2 refactor

--- src/main/java/pl/java/scalatech/config/JpaConfig.java
+++ src/main/java/pl/java/scalatech/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package pl.java.scalatech.config;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,10 +11,12 @@ import net.sf.log4jdbc.Log4jdbcProxyDataSource;
 import net.sf.log4jdbc.tools.Log4JdbcCustomFormatter;
 import net.sf.log4jdbc.tools.LoggingType;
 
+import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -49,35 +52,39 @@ public class JpaConfig {
 
     @Value("${dataSource.driverClassName}")
     private String driver;
-    
+
     @Value("${dataSource.url}")
     private String url;
-    
+
     @Value("${dataSource.username}")
     private String username;
-    
+
     @Value("${dataSource.password}")
     private String password;
-    
+
     @Value("${hibernate.dialect}")
     private String dialect;
-    
+
     @Value("${hibernate.hbm2ddl.auto}")
     private Boolean hbm2ddlAuto;
-    
+
     @Value("${boneCp.partition.count}")
     private int partitionCount;
-    
+
     @Value("${boneCp.partition.minConnectionsPerPartition}")
     private int minConnectionsPerPartition;
-    
+
     @Value("${boneCp.partition.maxConnectionsPerPartition}")
     private int maxConnectionsPerPartition;
-    
+
     @Value("${hibernate.show.sql}")
     private Boolean showSql;
 
+    @Value("${jpa.package}")
+    private String jpaPackage;
+
     @Bean(destroyMethod = "close")
+    @DependsOn("h2Server")
     public DataSource dataSourceOrginal() {
         BoneCPDataSource boneCPDataSource = new BoneCPDataSource();
         boneCPDataSource.setDriverClass(driver);
@@ -90,18 +97,32 @@ public class JpaConfig {
         return boneCPDataSource;
     }
 
-    @Bean
-    @Profile("profile")
-    public DataSource dataSource(DataSource orginalDs) {
-        Log4jdbcProxyDataSource dataSource = new Log4jdbcProxyDataSource(orginalDs);
-        dataSource.setLogFormatter(logFormater());
-        return dataSource;
+   /* @Bean
+    public Flyway flyway() {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSourceOrginal());
+        flyway.migrate();
+        return flyway;
+    }*/
+
+    @Bean(name = "h2Server")
+    Server h2Server() throws SQLException {
+        // jdbc:h2:tcp://localhost:9092/mem:test;DB_CLOSE_DELAY=-1
+        return Server.createTcpServer().start();
     }
 
     @Bean
-    @Profile(value={"dev","test"})
+    Server h2WebServer() throws SQLException {
+        // http://localhost:8082/
+        return Server.createWebServer().start();
+    }
+
+  
+
+    @Bean
+    @Profile(value = { "dev", "test" })
     public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL).build();
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
     }
 
     @Bean
@@ -116,24 +137,26 @@ public class JpaConfig {
 
     public Map<String, Object> jpaProperties() {
         Map<String, Object> props = new HashMap<>();
-   /*     props.put("hibernate.cache.use_query_cache", "true");
-        props.put("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
-        props.put("hibernate.cache.provider_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
-        props.put("hibernate.cache.use_second_level_cache", "true");*/
+        /*
+         * props.put("hibernate.cache.use_query_cache", "true");
+         * props.put("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
+         * props.put("hibernate.cache.provider_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
+         * props.put("hibernate.cache.use_second_level_cache", "true");
+         */
         return props;
     }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
-        if (Arrays.asList(env.getActiveProfiles()).containsAll(Lists.newArrayList("dev","test"))) {
-            lef.setDataSource(dataSource(dataSource()));
+        if (Arrays.asList(env.getActiveProfiles()).containsAll(Lists.newArrayList("dev", "test"))) {
+            lef.setDataSource(dataSource());
         } else {
-            lef.setDataSource(dataSource(dataSourceOrginal()));
+            lef.setDataSource(dataSourceOrginal());
         }
         lef.setJpaVendorAdapter(jpaVendorAdapter());
         lef.setJpaPropertyMap(jpaProperties());
-        lef.setPackagesToScan("pl.java.scalatech.entity"); // eliminate persistence.xml
+        lef.setPackagesToScan(jpaPackage); // eliminate persistence.xml
         return lef;
     }
 
@@ -143,7 +166,7 @@ public class JpaConfig {
         hibernateJpaVendorAdapter.setShowSql(showSql);
         hibernateJpaVendorAdapter.setGenerateDdl(hbm2ddlAuto);
         hibernateJpaVendorAdapter.setDatabase(Database.HSQL);
-        hibernateJpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.HSQLDialect");
+        hibernateJpaVendorAdapter.setDatabasePlatform(dialect);
         return hibernateJpaVendorAdapter;
     }
 
